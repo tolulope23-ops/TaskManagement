e5e4164f8b867021afb75d2d6296dbef1bf10e42
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-03-19 11:41:21 +0100

    feat: add validation middleware to route

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const { userRegister, userLogin, deleteUserAccount } = require('../controller/userAuth');
+const { validateUserData} = require('../middleware/validation');
 const router = express.Router();
 
-router.post('/register', userRegister);
-router.post('/login', userLogin);
+router.post('/register', validateUserData, userRegister);
+router.post('/login', validateUserData, userLogin);
 router.delete('/deleteAccount/:id', deleteUserAccount);
 
 
