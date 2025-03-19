ea1df7c732060a069fe79deaad6f170c87cec2c0
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-03-19 11:40:13 +0100

    feat: middleware for user fields validation

---
+++ middleware/validation.js
@@ -0,0 +1,20 @@
+const {StatusCodes} = require('http-status-codes');
+const {userRegisterData, userLoginData} = require('../utils/validation');
+
+const validateUserData = (req, res, next) => {
+    //Check if the user is registering or logging in to determin the schema validation type
+    const userAction = () => {
+        const SchemaType = req.body.firstname && req.body.lastname ? userRegisterData() : userLoginData();
+        return SchemaType;
+    }
+    const { error } = userAction().validate(req.body, {abortEarly: false});
+    if(error) {
+        return res.status(StatusCodes.BAD_REQUEST).json({
+        errors: error.details.map((e) => e.message)
+    });
+        }
+    next();
+};
+
+
+module.exports = {validateUserData};
