3d1b7165235b482e015ff379d7c30ceb86ab6d68
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-03-21 05:43:00 +0100

    feat: authentication middlware for delete user route

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const { userRegister, userLogin, deleteUserAccount } = require('../controller/userAuth');
 const { validateUserData} = require('../middleware/validation');
+const { isUserAuthenticated } = require('../middleware/isAuthentication');
 const router = express.Router();
 
 router.post('/register', validateUserData, userRegister);
 router.post('/login', validateUserData, userLogin);
-router.delete('/deleteAccount/:id', deleteUserAccount);
+router.delete('/deleteAccount/:id',isUserAuthenticated, deleteUserAccount);
 
 
 module.exports = router;
