a9044e21af25912099fe882a1e102ef78ee08121
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-04-25 10:30:31 +0100

    feat: added email middleware to user route

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -2,9 +2,10 @@ const express = require('express');
 const { userRegister, userLogin, deleteUserAccount } = require('../controller/userAuth');
 const { validateUserData} = require('../middleware/validation');
 const { isUserAuthenticated } = require('../middleware/isAuthentication');
+const { verifyAccount } = require('../middleware/sendEmail');
 const router = express.Router();
 
-router.post('/register', validateUserData, userRegister);
+router.post('/register', validateUserData, userRegister, verifyAccount);
 router.post('/login', validateUserData, userLogin);
 router.delete('/deleteAccount/:id',isUserAuthenticated, deleteUserAccount);
 
