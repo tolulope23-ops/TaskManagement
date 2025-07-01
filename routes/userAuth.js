b6f203a61fc0c26489bf5574a05c1382b2055c52
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-07-01 19:36:13 +0100

    feat: add send email verification to route

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -2,11 +2,12 @@ const express = require('express');
 const { userRegister, userLogin} = require('../controller/userAuth');
 const { validateUserData} = require('../middleware/validation');
 const { isUserAuthenticated } = require('../middleware/isAuthentication');
-const { verifyEmail } = require('../middleware/sendEmail');
+const { sendVerification } = require('../utils/sendverificationEmail');
 const router = express.Router();
 
-router.post('/register', validateUserData, userRegister, verifyEmail);
-router.post('/login', validateUserData, userLogin);
+router.post('/register', validateUserData, userRegister, sendVerification);
+// router.post('/verifyEmail', verifyEmail)
+router.post('/login', validateUserData, isUserAuthenticated, userLogin);
 // router.delete('/deleteAccount/:id',isUserAuthenticated, deleteUserAccount);
 
 
