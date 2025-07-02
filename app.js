dabd5563955132fdf8fb606431c4b242a13ffa0f
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-07-02 23:58:50 +0100

    feat: proper varibale naming

--- app.js
+++ app.js
@@ -6,13 +6,18 @@ const cors = require('cors');
 const connectDB = require('./db/dbConfig');
 const userRoute = require('./routes/userAuth');
 const userProfileRoute = require('./routes/userProfile');
+const taskRoute = require('./routes/tasks');
+
 const { errorHandlerMiddlware } = require('./middleware/errorHandler');
 
 app.use(cors());
 app.use(express.json());
+const BASE_URL = '/api/v1';
 
-app.use('/api/v1/', userRoute);
-app.use('/api/v1/', userProfileRoute);
+app.use(`${BASE_URL}/auth`, userRoute);
+app.use(`${BASE_URL}/profile`, userProfileRoute);
+app.use(`${BASE_URL}/task`, taskRoute);
+
 app.use(errorHandlerMiddlware);
 
 const PORT = process.env.PORT || 3000;
