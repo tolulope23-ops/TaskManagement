5fd58cbe9c6f3309e2456b6108508edea1805467
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-07-02 23:59:55 +0100

    arangement

--- routes/userAuth.js
+++ routes/userAuth.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const router = express.Router();
+
 const { userRegister, userLogin} = require('../controller/userAuth');
 const { validateUserData} = require('../middleware/validation');
 const { isUserAuthenticated } = require('../middleware/isAuthentication');
 const { sendVerification } = require('../utils/sendverificationEmail');
-const router = express.Router();
+const { verifyUserEmail } = require('../middleware/verifyEmail');
+
 
 router.post('/register', validateUserData, userRegister, sendVerification);
-// router.post('/verifyEmail', verifyEmail)
+router.post('/verifyEmail', verifyUserEmail);
+router.post('/resendOTP', sendVerification);
 router.post('/login', validateUserData, isUserAuthenticated, userLogin);
 // router.delete('/deleteAccount/:id',isUserAuthenticated, deleteUserAccount);
 
