5f692c5ef8e3f4dafc87202043bfebae7fecdc71
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-07-03 22:55:12 +0100

    feat: add fnction to routes

--- routes/tasks.js
+++ routes/tasks.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createTask, deleteTask } = require('../controller/tasks');
+const { createTask, deleteTask, getTask, getAllTask, updateTask } = require('../controller/tasks');
 
 router.post('/', createTask);
+router.get('/:id', getTask);
+router.get('/tasks', getAllTask);
+router.put('/update/:id', updateTask);
 router.delete('/delete/:id', deleteTask);
 
 module.exports = router;
