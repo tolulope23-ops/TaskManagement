4628d02695c2b7982633b5fd95586d680024e69a
Adeyemi Rachael <adeyemitoluadedoyin@gmail.com>
2025-07-03 06:46:19 +0100

    feat: delete task controller

--- controller/tasks.js
+++ controller/tasks.js
@@ -1,10 +1,11 @@
+// NB: Proper error handling
 const {StatusCodes} = require('http-status-codes');
 const Task = require('../model/tasks');
 
 const createTask = async (req, res, next) => {
     try {
         const create_task = await Task.create(req.body);
-        res.status(StatusCodes.CREATED).json({
+        return res.status(StatusCodes.CREATED).json({
             sucess: true,
             message: 'Task(s) created successfuly',
             data: create_task
@@ -15,8 +16,24 @@ const createTask = async (req, res, next) => {
 };
 
 
+const deleteTask = async (req, res, next) => {
+    const { id } = req.params;
+    try {
+        const delete_task = await Task.findByIdAndDelete(id);
+        if(!delete_task){
+            return res.status(StatusCodes.NOT_FOUND).json({
+                success: false,
+                message: 'Task with the ID not found'
+            });
+        }
+        return res.status(StatusCodes.OK).json({
+            success: true,
+            message: 'Task deleted'
+        });
+    } catch (error) {
+        next(error)
+    }
+}
 
 
-
-
-module.exports = {createTask};
+module.exports = {createTask, deleteTask};
